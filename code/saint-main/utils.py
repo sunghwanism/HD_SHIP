be8568a617a88225574999dfc1832e892740b629
Sunghwan Moon <msh2044@gmail.com>
2023-10-12 12:06:47 +0900

    load SAINT model and fit our data

---
+++ code/saint-main/utils.py
@@ -0,0 +1,177 @@
+import os
+
+import torch
+from sklearn.metrics import roc_auc_score, mean_squared_error
+import numpy as np
+from augmentations import embed_data_mask
+import torch.nn as nn
+import pandas as pd
+from tqdm import tqdm
+
+from sklearn.preprocessing import LabelEncoder
+import lightgbm as lgb
+import bisect
+
+import random
+
+def make_default_mask(x):
+    mask = np.ones_like(x)
+    mask[:,-1] = 0
+    return mask
+
+def tag_gen(tag,y):
+    return np.repeat(tag,len(y['data']))
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)  
+
+def get_scheduler(args, optimizer):
+    if args.scheduler == 'cosine':
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs)
+    elif args.scheduler == 'linear':
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+                                      milestones=[args.epochs // 2.667, args.epochs // 1.6, args.epochs // 1.142], gamma=0.1)
+    return scheduler
+
+def imputations_acc_justy(model,dloader,device):
+    model.eval()
+    m = nn.Softmax(dim=1)
+    y_test = torch.empty(0).to(device)
+    y_pred = torch.empty(0).to(device)
+    prob = torch.empty(0).to(device)
+    with torch.no_grad():
+        for i, data in enumerate(dloader, 0):
+            x_categ, x_cont, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device)
+            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model)
+            reps = model.transformer(x_categ_enc, x_cont_enc)
+            y_reps = reps[:,model.num_categories-1,:]
+            y_outs = model.mlpfory(y_reps)
+            # import ipdb; ipdb.set_trace()   
+            y_test = torch.cat([y_test,x_categ[:,-1].float()],dim=0)
+            y_pred = torch.cat([y_pred,torch.argmax(m(y_outs), dim=1).float()],dim=0)
+            prob = torch.cat([prob,m(y_outs)[:,-1].float()],dim=0)
+     
+    correct_results_sum = (y_pred == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]*100
+    auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
+    return acc, auc
+
+
+def multiclass_acc_justy(model,dloader,device):
+    model.eval()
+    vision_dset = True
+    m = nn.Softmax(dim=1)
+    y_test = torch.empty(0).to(device)
+    y_pred = torch.empty(0).to(device)
+    prob = torch.empty(0).to(device)
+    with torch.no_grad():
+        for i, data in enumerate(dloader, 0):
+            x_categ, x_cont, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device)
+            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model,vision_dset)
+            reps = model.transformer(x_categ_enc, x_cont_enc)
+            y_reps = reps[:,model.num_categories-1,:]
+            y_outs = model.mlpfory(y_reps)
+            # import ipdb; ipdb.set_trace()   
+            y_test = torch.cat([y_test,x_categ[:,-1].float()],dim=0)
+            y_pred = torch.cat([y_pred,torch.argmax(m(y_outs), dim=1).float()],dim=0)
+     
+    correct_results_sum = (y_pred == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]*100
+    return acc, 0
+
+
+def classification_scores(model, dloader, device, task,vision_dset):
+    model.eval()
+    m = nn.Softmax(dim=1)
+    y_test = torch.empty(0).to(device)
+    y_pred = torch.empty(0).to(device)
+    prob = torch.empty(0).to(device)
+    with torch.no_grad():
+        for i, data in enumerate(dloader, 0):
+            x_categ, x_cont, y_gts, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device),data[4].to(device)
+            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model,vision_dset)           
+            reps = model.transformer(x_categ_enc, x_cont_enc)
+            y_reps = reps[:,0,:]
+            y_outs = model.mlpfory(y_reps)
+            # import ipdb; ipdb.set_trace()   
+            y_test = torch.cat([y_test,y_gts.squeeze().float()],dim=0)
+            y_pred = torch.cat([y_pred,torch.argmax(y_outs, dim=1).float()],dim=0)
+            if task == 'binary':
+                prob = torch.cat([prob,m(y_outs)[:,-1].float()],dim=0)
+     
+    correct_results_sum = (y_pred == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]*100
+    auc = 0
+    if task == 'binary':
+        auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
+    return acc.cpu().numpy(), auc
+
+def mean_sq_error(model, dloader, device, vision_dset):
+    model.eval()
+    y_test = torch.empty(0).to(device)
+    y_pred = torch.empty(0).to(device)
+    with torch.no_grad():
+        for i, data in enumerate(dloader, 0):
+            x_categ, x_cont, y_gts, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device),data[4].to(device)
+            _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model,vision_dset)           
+            reps = model.transformer(x_categ_enc, x_cont_enc)
+            y_reps = reps[:,0,:]
+            y_outs = model.mlpfory(y_reps)
+            y_test = torch.cat([y_test,y_gts.squeeze().float()],dim=0)
+            y_pred = torch.cat([y_pred,y_outs],dim=0)
+        # import ipdb; ipdb.set_trace() 
+        rmse = mean_squared_error(y_test.cpu(), y_pred.cpu(), squared=False)
+        return rmse
+
+
+def preprocessing(train, test):
+    # datetime 컬럼 처리
+    train['ATA'] = pd.to_datetime(train['ATA'])
+    test['ATA'] = pd.to_datetime(test['ATA'])
+
+    # datetime을 여러 파생 변수로 변환
+    for df in [train, test]:
+        df['year'] = df['ATA'].dt.year
+        df['month'] = df['ATA'].dt.month
+        df['day'] = df['ATA'].dt.day
+        df['hour'] = df['ATA'].dt.hour
+        df['minute'] = df['ATA'].dt.minute
+        df['weekday'] = df['ATA'].dt.weekday
+
+    # datetime 컬럼 제거
+    train.drop(columns='ATA', inplace=True)
+    test.drop(columns='ATA', inplace=True)
+    train.drop(columns='ID', inplace=True)
+    test.drop(columns='ID', inplace=True)
+
+    # Categorical 컬럼 인코딩
+    categorical_features = ['ARI_CO', 'ARI_PO', 'SHIP_TYPE_CATEGORY', 'SHIPMANAGER', 'FLAG']
+    encoders = {}
+
+    for feature in tqdm(categorical_features, desc="Encoding features"):
+        le = LabelEncoder()
+        train[feature] = le.fit_transform(train[feature].astype(str))
+        le_classes_set = set(le.classes_)
+        test[feature] = test[feature].map(lambda s: '-1' if s not in le_classes_set else s)
+        le_classes = le.classes_.tolist()
+        bisect.insort_left(le_classes, '-1')
+        le.classes_ = np.array(le_classes)
+        test[feature] = le.transform(test[feature].astype(str))
+        encoders[feature] = le
+
+    # 결측치 처리
+    train.fillna(train.mean(), inplace=True)
+    test.fillna(train.mean(), inplace=True)
+    
+    return train, test, categorical_features
+
+
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    # torch.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)  # type: ignore
+    # torch.backends.cudnn.deterministic = True  # type: ignore
+    # torch.backends.cudnn.benchmark = True  # type: ignore
